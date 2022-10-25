7c3c588625e3cf4c3d994c687b44af30e762aee1
Shah-Md-Jihan <shahmohammadjihan@gmail.com>
2022-10-25 18:15:22 +0600

    category data display on category page and footer component added

--- src/components/Categories/Categories.js
+++ src/components/Categories/Categories.js
@@ -1,11 +1,32 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
+import Card from 'react-bootstrap/Card';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import Container from 'react-bootstrap/Container';
+import './Category.css'
 
 const Categories = () => {
     const categories = useLoaderData();
     return (
-        <div>
-            <h3>This is Categories{categories.length}</h3>
+        <div className='mt-5'>
+            <Container>
+                <h3 className='mb-5 fs-2 text-info'><span className='text-info fs-1 fw-bold'>| </span>Select the language that you want to learn :</h3>
+                <Row xs={1} md={2} className="g-4 text-center">
+                    {categories.map(category => <Col key={category._id}>
+                        <Card>
+                            <Card.Img variant="top" className='category_img p-5 w-75 mx-auto' src={category.img} />
+                            <Card.Body>
+                                <Card.Title className='fs-2'>{category.title}</Card.Title>
+                                <Card.Text className='fs-4 pb-5'>
+                                    {category.description}
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    )}
+                </Row>
+            </Container>
         </div>
     );
 };
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div className='bg-dark p-5 mt-5'>
+            <p className='text-danger text-center'>Copyright &copy; 2025 | E-Learning Hub BD.</p>
+        </div>
+    );
+};
+
+export default Footer;
